d5b114c79d83e4292390b8b6697334cae31e95f8
Sandesh Choudhary <sandeshchoudhary5@gmail.com>
2020-04-11 11:59:39 +0530

    chore: init for gh-pages

---
+++ precache-manifest.637df483da67eba701463fa2be737fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c56a6e086c46b5961298c5809e5081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5120ffe8a27864e3402",
+    "url": "/static/css/main.7217688c.chunk.css"
+  },
+  {
+    "revision": "e13e727ffb9066892821",
+    "url": "/static/js/2.82890efb.chunk.js"
+  },
+  {
+    "revision": "53c72b154f62dd8d5fae3fac1bfdfb03",
+    "url": "/static/js/2.82890efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5120ffe8a27864e3402",
+    "url": "/static/js/main.442314bb.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/static/js/runtime-main.82a2a924.js"
+  }
+]);
--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-import ApolloClient from 'apollo-boost';
-
-export const client = new ApolloClient({
-  uri: 'https://covid-tracker-news-graphql.aregee.now.sh/',
-});
-
