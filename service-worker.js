f9e459a0ec86c5c8bb355f6645dd02c5f5381407
sandeshchoudhary <sandeshchoudhary5@gmail.com>
2020-05-02 06:33:09 +0000

    Deploying to gh-pages from  @ 19d60947de89a5d5281bf3bde296c2fc980744fe 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid/precache-manifest.fffe117a2f28aeef330850ce643b4634.js"
+  "/precache-manifest.2788b53158cfb2a7edbf927d2986bbcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
