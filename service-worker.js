6cb5fb081b0484d4b7ef938ac9b68733120401e0
sandeshchoudhary <sandeshchoudhary5@gmail.com>
2020-05-18 03:18:59 +0000

    Deploying to gh-pages from  @ f06711144ad57488aa029c9979f50e632c2e819b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.391e0c0f8079cee4cad7ec3d45a6a924.js"
+  "/precache-manifest.a420b4d124144f8352a0111c9880f3ba.js"
 );
 
 self.addEventListener('message', (event) => {
